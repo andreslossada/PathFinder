f80c6b646a6ff949a01fc5573d05e9c3006c140b
dresan22 <andresat48@hotmail.com>
2023-05-24 00:27:53 -0400

    vite

--- src/utils/service.js
+++ src/utils/service.js
@@ -5,7 +5,6 @@ import {
 } from "openai";
 import { INITIAL_MESSAGES } from "./prompts";
 
-
 export const processMessageToChatGPT = async (answers) => {
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_OPENAI_API_KEY,
