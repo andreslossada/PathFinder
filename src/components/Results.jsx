4b5f1ebfa0dd012c39550d3ae9ed2c7417643751
dresan22 <andresat48@hotmail.com>
2023-05-27 21:38:36 -0400

    add dots to searching

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,9 +2,21 @@ import Fade from "react-reveal/Fade";
 import { useStore } from "../store/zustand";
 import CardSkeleton from "./CardSkeleton";
 import { mock } from "../store/mocks";
+import { useEffect, useState } from "react";
 
 export default function Results() {
   const results = useStore((state) => state.results);
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDots((prevDots) => (prevDots === "..." ? "" : prevDots + "."));
+    }, 300);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   return (
     <Fade bottom cascade>
       <div className=" grid  place-content-center w-[95vw] xl:w-[70vw] ">
@@ -18,7 +30,10 @@ export default function Results() {
           })}
         {results?.results.length === 0 && (
           <Fade bottom delay={1000}>
-            <p className="text-lg text-white text-center">Searching...</p>
+            <p className="text-lg text-white text-center relative">
+              Searching <strong className="absolute"> {dots}</strong>
+            </p>
+
             <p className="text-lg text-gray-400 text-center">
               This can take several seconds
             </p>
